8ea6ec7a346e4d981d16688f3d8d0f1e949827a4
sensiz <dev.federicosensi@gmail.com>
2022-10-11 17:50:39 +0200

    mail ex complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 
+const exDadi = document.getElementById('dadi');
+
 
 const signedMail = ['hello@mail.com', 'world@mail.com'];
 
-
-
+const inputNone = document.getElementById('mail');
+const buttonNone = document.getElementById('button');
+const buttonDadi = document.getElementById('dadiButton');
+buttonDadi.style.display = 'none';
 
 for (let j = 0; j < signedMail.length; j++) {
 
@@ -16,11 +20,13 @@ for (let j = 0; j < signedMail.length; j++) {
         if (inputMail !== signedMail[j]) {
             console.log('no');
             alert('Email errata, riprova.');
-
-        } else {
+            
+        } else if (inputMail === signedMail[j]) {
             console.log('si');
             alert('Email corretta,');
-            j = signedMail.length;
+            inputNone.style.display = 'none';
+            buttonNone.style.display = 'none';
+            buttonDadi.style.display = 'block';
         }
         alert = function () {};
     })
